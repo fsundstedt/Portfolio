e42068cb4110d7ba81b6b8e38024dfde7432f3d2
Frank Sundstedt <frank.sundstedt@gmail.com>
2020-03-20 10:58:48 -0400

    added numerous changes. No JS yet

---
+++ scripts/scripts-about.js
@@ -0,0 +1,8 @@
+function codingFunction() {
+    let codingButton = document.getElementById("about-coding-button");
+    if (codingButton.style.color === "white") {
+        codingButton.style.color === "black";
+    } else {
+        codingButton.style.color === "green";
+    }
+}
